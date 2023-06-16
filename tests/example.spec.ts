166c68c2835583607a7e180db4ffff71cbd64171
lucashdoa <lucashdoa@gmail.com>
2023-06-15 23:47:27 -0300

    Add first tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByPlaceholder('What needs to be done?').click();
+  await page.getByPlaceholder('What needs to be done?').fill('newtodo');
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+  await page.getByRole('checkbox', { name: 'Toggle Todo' }).check();
+  await page.locator('html').click();
+  await page.getByRole('button', { name: 'Clear completed' }).click();
+});
